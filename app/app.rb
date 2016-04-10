9d8ba625d2e26214ae966fbed1351c9aa6cbee08
Nathan Menge <mengen@turnitin.com>
2016-04-10 14:49:28 -0400

    Settled Stat identification.

--- app/app.rb
+++ app/app.rb
@@ -11,21 +11,26 @@ require 'db'
 $LOAD_PATH.unshift(File.expand_path(File.join('app', 'api')))
 require 'bone'
 
+# This module holds all code relating to the Service
 module Skeleton
   # This class defined the API root for this service
   class API < Grape::API
     use Grape::Middleware::Logger, logger: LOGGER
-    StatsD.prefix = downcase.gsub(/::/, '__')
+    local_name = name
 
-    version 'v0', using: :path
     prefix 'api'
-    format :json
-
     mount ::Skeleton::Bones
 
+    version 'v0', using: :path
+    format :json
+
     get '/ping' do
-      StatsD.increment 'ping'
-      StatsD.measure('ping') { { data: 'pong' } }
+      StatsD.increment Skeleton.stats_string(local_name, 'ping')
+      StatsD.measure(Skeleton.stats_string(local_name, 'ping')) { { data: 'pong' } }
     end
+  end
+
+  def self.stats_string(name, action)
+    name.gsub('::', '.') + '.' + action
   end
 end
--- spec/api/bones_spec.rb
+++ spec/api/bones_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Skeleton::Bones do
   end
 
   it 'creates' do
-    post 'api/v0/bones/', { name: 'create_test'}.to_json, "CONTENT_TYPE" => "application/json"
+    post 'api/v0/bones/', { name: 'create_test' }.to_json, 'CONTENT_TYPE' => 'application/json'
     bond.spy(last_response.status)
     bond.spy(Bone.first.name)
   end
 
   it 'indexes' do
-    Bone.create({ name: 'index1' })
-    Bone.create({ name: 'index2' })
+    Bone.create(name: 'index1')
+    Bone.create(name: 'index2')
     get 'api/v0/bones/'
     bond.spy(last_response.status)
     bond.spy(JSON.parse(last_response.body))
   end
 
   it 'performs a lookup' do
-    Bone.create({ name: 'lookup' })
+    Bone.create(name: 'lookup')
     id = Bone.first.id
     get "api/v0/bones/#{id}"
     bond.spy(last_response.status)
@@ -29,19 +29,18 @@ RSpec.describe Skeleton::Bones do
   end
 
   it 'updates' do
-    Bone.create({ name: 'not_updated'})
+    Bone.create(name: 'not_updated')
     id = Bone.first.id
-    put "api/v0/bones/#{id}", { name: 'updated' }.to_json, "CONTENT_TYPE" => "application/json"
+    put "api/v0/bones/#{id}", { name: 'updated' }.to_json, 'CONTENT_TYPE' => 'application/json'
     bond.spy(last_response.status)
     bond.spy(Bone.first.name)
   end
 
   it 'deletes' do
-    Bone.create({ name: 'not_deleted'})
+    Bone.create(name: 'not_deleted')
     id = Bone.first.id
     delete "api/v0/bones/#{id}"
     bond.spy(last_response.status)
     bond.spy(Bone.count)
   end
-
 end
